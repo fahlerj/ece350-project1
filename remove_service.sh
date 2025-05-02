069b1a65ed16db5d25e6ffea73646d51507e8642
Jarod Fahler <jarodfahler@me.com>
2025-05-02 00:59:12 -0400

    Adding a quality of life improvement

--- remove_service.sh
+++ remove_service.sh
@@ -7,3 +7,4 @@ sudo systemctl disable start_temp.service
 sudo rm -f /lib/systemd/system/start_temp.service
 sudo rm -rvf /opt/ece350_project
 sudo systemctl daemon-reload
+git restore start_temp.service

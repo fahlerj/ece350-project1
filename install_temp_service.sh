72a7ae8ff7060b086b12e3dc76431ddf11e7c45b
Jarod Fahler <jarodfahler@me.com>
2025-05-02 00:54:37 -0400

    Squashing some more bugs on service side

--- install_temp_service.sh
+++ install_temp_service.sh
@@ -3,11 +3,16 @@
 echo "This will install the service file to the system"
 set -x
 
-chmod 777 ./project_jfahler_chooks.py
+dest_dir="/opt/ece350_project"
+
+project_py_file="project_jfahler_chooks.py"
+
+chmod 777 $project_py_file
+sudo rsync -av $project_py_file $dest_dir/
 
 # Update user-specific values
 sed -i s,REPLACE_USER,`whoami`,g ./start_temp.service
-sed -i s,REPLACE_DIR,`pwd`,g ./start_temp.service
+sed -i s,REPLACE_DIR,$dest_dir,g ./start_temp.service
 
 # Install the service file
 sudo cp -av ./start_temp.service /lib/systemd/system/
--- remove_service.sh
+++ remove_service.sh
@@ -5,4 +5,5 @@ set -x
 sudo systemctl stop start_temp.service
 sudo systemctl disable start_temp.service
 sudo rm -f /lib/systemd/system/start_temp.service
+sudo rm -rvf /opt/ece350_project
 sudo systemctl daemon-reload

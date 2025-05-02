18231420bf77905fa6ea8d07bf9fbc4cddae1467
Jarod Fahler <jarodfahler@me.com>
2025-05-02 00:03:46 -0400

    Update with the service files to install and run on boot

---
+++ install_temp_service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "This will install the service file to the system"
+set -x
+
+cp -av ./project_jfahler_chooks.py /tmp/
+chmod 777 /tmp/project_jfahler_chooks.py
+
+# Update user-specific values
+sed -i s/REPLACE_USER/`whoami`/g ./start_temp.service
+
+# Install the service file
+sudo cp -av ./start_temp.service /lib/systemd/system/
+
+# Reload systemd to re-read new service
+sudo systemctl daemon-reload
+
+# Start the service
+sudo systemctl start start_temp.service
+
+# Enable the service on boot
+sudo systemctl enable start_temp.service
